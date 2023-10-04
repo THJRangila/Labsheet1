69caddfb9e9e66bacf968b38f1c60dbf8b30daa6
THJ Rangila <139892889+THJRangila@users.noreply.github.com>
2023-10-04 16:01:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+    String a="Sam";
+    String b="John";
+    public static void main (String []args){
+        Student myObj1 = new Student();
+        Student myObj2 = new Student();
+        System.out.println(myObj1.a);
+        System.out.println(myObj2.b);
+    }
+    
+}
